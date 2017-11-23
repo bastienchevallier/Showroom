d4e21266ef5d53bd0c253c6b149ec22d954d3b24
bastienchevallier <32243212+bastienchevallier@users.noreply.github.com>
2017-11-23 19:25:35 +0100

    Revert "v0 front that is working"
    This reverts commit 9fa222c6f1b6c95fae8ee76368c893581ea62dd9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,8 @@ import {ShowListPage} from '../pages/show-list/show-list';
 import {ShowDetailPage} from '../pages/show-detail/show-detail';
 import {FavoriteListPage} from '../pages/favorite-list/favorite-list';
 import {AboutPage} from '../pages/about/about';
-<<<<<<< HEAD
 import {QrScannerPage} from '../pages/qr-scanner/qr-scanner';
 import {DealService} from "../providers/deal-service-rest";
-=======
-
-import {ShowService} from "../providers/show-service-rest";
->>>>>>> parent of 9fa222c6... v0 front that is working
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -25,16 +20,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     WelcomePage,
     AboutPage,
-<<<<<<< HEAD
     DealListPage,
     DealDetailPage,
     FavoriteListPage,
     QrScannerPage
-=======
-    ShowListPage,
-    ShowDetailPage,
-    FavoriteListPage
->>>>>>> parent of 9fa222c6... v0 front that is working
   ],
   imports: [
     BrowserModule,
@@ -46,27 +35,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     WelcomePage,
     AboutPage,
-<<<<<<< HEAD
     DealListPage,
     DealDetailPage,
     FavoriteListPage,
     QrScannerPage
-=======
-    ShowListPage,
-    ShowDetailPage,
-    FavoriteListPage
->>>>>>> parent of 9fa222c6... v0 front that is working
   ],
   providers: [
     StatusBar,
     SplashScreen,
-<<<<<<< HEAD
     DealService,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-=======
-    ShowService,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
->>>>>>> parent of 9fa222c6... v0 front that is working
   ]
 })
 export class AppModule {}
